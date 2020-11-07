5c4f7baf86077e8539a62059ecddfaae060ca63b
Chery Sun <alittlemoyu@gmail.com>
2020-11-07 15:09:16 +0800

    Version 0.9
    写不出来圆的代码，就这样吧

--- Drive/Main.ino
+++ Drive/Main.ino
@@ -9,7 +9,7 @@
 #define STEPPER2_DIR_PIN 4
 #define STEPPER2_STEP_PIN 5
 
-//Servo myservo;
+Servo myServo;
 
 AccelStepper stepper1(AccelStepper::DRIVER, STEPPER1_STEP_PIN, STEPPER1_DIR_PIN);
 AccelStepper stepper2(AccelStepper::DRIVER, STEPPER2_STEP_PIN, STEPPER2_DIR_PIN);
@@ -22,18 +22,14 @@ boolean EOAS = false;
 char answer;
 
 long positions[2]; // Array of desired stepper positions
-float siny;
 int count = 0;
-float tobeacosed;
-float arccos;
-float sindata;
 
 void setup()
 {
   Serial.begin(115200);
   stepper1.setMaxSpeed(100);
   stepper2.setMaxSpeed(100);
-  //  myservo.attach(6);
+  myServo.attach(6);
   steppers.addStepper(stepper1);
   steppers.addStepper(stepper2);
   positions[0] = 0;
@@ -42,18 +38,7 @@ void setup()
 
 void loop()
 {
-  //  if (count < 1600){
-  //    count=count+1;
-  //    positions[0]=count;
-  //    Serial.println(positions[0]/1600.0);
-  //    tobeacosed = (1600-count)/1600.0;
-  //    arccos = acos(tobeacosed);
-  //    sindata = sin(arccos)*1600;
-  //    positions[1]= int(sindata);
-  //    Serial.println(positions[1]);
-  //    steppers.moveTo(positions);
-  //    steppers.runSpeedToPosition();
-  //  }
+
   delay(100);
   SerialFunc();
 }
--- Drive/Print.ino
+++ Drive/Print.ino
@@ -1,11 +1,35 @@
+#include <Main.ino>
+
 void printLine(){
-  //servo
-  //motor
-  //servo
-  //motor
-  //servo
+  myServo.write(30);
+  positions[0] = floatX[0]*1600/4;
+  positions[1] = floatY[0]*1600/4;
+  steppers.moveTo(positions);
+  steppers.runSpeedToPosition();
+  myServo.write(0);
+  positions[0] = floatX[1]*1600/4;
+  positions[1] = floatY[1]*1600/4; 
+  steppers.moveTo(positions);
+  steppers.runSpeedToPosition();
+  myServo.write(30);
 }
 
 void printArc(){
-  
+  // myServo.write(30);
+  // positions[0] = (floatCircleX+floatCircleR*cos(theta*PI))*1600/4;
+  // positions[1] = (floatCircleY+floatCircleR*sin(theta*PI))*1600/4;
+  // steppers.moveTo(positions);
+  // steppers.runSpeedToPosition();
+  // myServo.write(0);
+  // if (count < floatCircleR*((gamma-theta)/(PI/2))*1600/4){
+  //   count=count+1;
+  //   positions[0]=count;
+  //   toBeAcosed = ((floatCircleR*1600/4-count)/(floatCircleR*1600/4);
+  //   arccos = acos(toBeAcosed);
+  //   sindata = sin(arccos)*floatCircleR;
+  //   positions[1]= int(sindata);
+  //   steppers.moveTo(positions);
+  //   steppers.runSpeedToPosition();
+  // }
+  //WIP
 }
